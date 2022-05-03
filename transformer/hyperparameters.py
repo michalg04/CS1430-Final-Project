63bf0d4b51d87ffa9c5ae5d42a2da5122d764d0d
Catherine Chen <catherine_s_chen@brown.edu>
2022-05-03 14:02:21 -0400

    change hyperparams

--- transformer/hyperparameters.py
+++ transformer/hyperparameters.py
@@ -1,6 +1,6 @@
 learning_rate = 0.001
 weight_decay = 0.0001
-batch_size = 128
+batch_size = 256
 num_epochs = 100
 image_size = 72  # We'll resize input images to this size
 patch_size = 6  # Size of the patches to be extract from the input images
@@ -12,6 +12,6 @@ transformer_units = [
     projection_dim,
 ]  # Size of the transformer layers
 transformer_layers = 8
-mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
+mlp_head_units = [512, 256]  # Size of the dense layers of the final classifier
 num_classes = 2
 input_shape = (72, 72)
